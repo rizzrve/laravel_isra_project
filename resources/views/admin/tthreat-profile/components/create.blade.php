6a719ac10fb9114266d5c0e33d19f7799e485cc0
Rian <rianiskandar123@gmail.com>
2024-10-06 00:55:15 +0800

    UI Changes to Threat Module
    Just revamped the threats module. Still not fully done yet as there are some submodules that needs to be intergrated to the new UI

--- resources/views/admin/tthreat-profile/components/create.blade.php
+++ resources/views/admin/tthreat-profile/components/create.blade.php
@@ -10,32 +10,39 @@
                 <div class="btn-group" role="group">
                     <nav>
                         <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                            <button class="nav-link active" id="nav-create-threat" data-bs-toggle="tab"
-                                data-bs-target="#nav-create-threat" type="button" role="tab"
-                                aria-controls="nav-create-threat" aria-selected="true">
-                                Group
+                            <button class="nav-link active" id="nav-group-tab" data-bs-toggle="tab"
+                                data-bs-target="#nav-group" type="button" role="tab" aria-controls="nav-group"
+                                aria-selected="true">
+                                Create Group
                             </button>
-                            <button class="nav-link" id="nav-create-group" data-bs-toggle="tab"
-                                data-bs-target="#nav-create-group" type="button" role="tab"
-                                aria-controls="nav-create-group" aria-selected="false">
-                                Threat
+                            <button class="nav-link" id="nav-threat-tab" data-bs-toggle="tab"
+                                data-bs-target="#nav-threat" type="button" role="tab" aria-controls="nav-threat"
+                                aria-selected="false">
+                                Create Threat
                             </button>
                         </div>
                     </nav>
-                    <div class="tab-content" id="nav-tab">
-                        <div class="tab-pane fade show active" id="nav-create-group" role="tabpanel"
-                            aria-labelledby="nav-create-group" tabindex="0">
-                            ...
-                        </div>
-                        <div class="tab-pane fade show active" id="nav-tab" role="tabpanel"
-                            aria-labelledby="nav-create-threat" tabindex="0">
-                            ...
-                        </div>
-                       
-                    </div>
                 </div>
             </div>
+        </div>
 
+        <div class="d-flex justify-content-center align-items-center my-3">
+            <div class="tab-content" id="nav-tabContent">
+                <div class="tab-pane fade show active" id="nav-group" role="tabpanel" aria-labelledby="nav-group-tab"
+                    tabindex="0">
+                    <form action="{{ route('threat-groups.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Group Name</label>
+                            <input type="text" class="form-control" id="name" name="name" required>
+                        </div>
+                    </form>
+                </div>
+                <div class="tab-pane fade" id="nav-threat" role="tabpanel" aria-labelledby="nav-threat-tab"
+                    tabindex="0">
+                    
+                </div>
+            </div>
         </div>
     </div>
 
